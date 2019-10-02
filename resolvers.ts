43afd422ea342870f0c72617139cbd9ffd43b397
Thomas Sileghem <th.sileghem@gmail.com>
2019-10-02 12:29:06 +0100

    initial commit

---
+++ resolvers.ts
@@ -0,0 +1,23 @@
+import { Resolvers } from "./typings";
+
+const resolvers: Resolvers = {
+  Query: {
+    users() {
+      return [
+        {
+          id: '1',
+          firstName: 'John',
+          lastName: 'Smith',
+          // Property 'fullName' is missing in type '{ id: string; firstName: string; lastName: string; }' but required in type 'User'.
+        }
+      ]
+    }
+  },
+  User: {
+    fullName(user) {
+      return `${user.firstName} ${user.lastName}`;
+    }
+  }
+}
+
+export default resolvers
